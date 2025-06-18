839602da562247a1a1d2a1cfa22f791444296c8a
LeTienLoc <loc.le1@ntq-solution.com.vn>
2025-06-18 17:47:51 +0700

    add drop table

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+if [ "$DELETE_DB" = "true" ]; then
+  echo "🧨 DELETE_DB=true → Clearing DB via raw SQL..."
+  pnpx prisma db execute --file=./scripts/clear-db.sql --schema=./prisma/schema.prisma
+else
+  echo "ℹ️ Skipping DB clear (DELETE_DB != true)"
+fi
+
 pnpx prisma migrate deploy && node server.js
